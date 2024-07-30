48df99db267601ecdf39095dffa762710d21f61d
Maurice <Tysons3x3@gmail.com>
2024-07-29 20:30:28 -0400

    npm i prop-types into project

---
+++ src/Button.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Button({children,
+  primary, secondary, success, warning, danger, outline, rounded
+}) {
+
+  return (
+    <button >
+     {children}
+    </button>
+  )
+}
+
+export default Button
